e36643ffe72c536be249a9337c313852fb17b33e
Robin Beer <contact@robin-beer.com>
2022-12-15 22:25:29 +0100

    make available as pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,42 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>VBB Departures</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#DC2626" />
+
+        <meta name="description" content="An app to see VBB departures including real time delays." />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="keywords" content="VBB, Public Transport, Berlin, ÖPNV" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 };
 
 export default trpc.withTRPC(MyApp);
