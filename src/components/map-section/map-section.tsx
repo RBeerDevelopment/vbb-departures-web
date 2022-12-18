0fa1b70a3f308b16f7a98bfaa84351bba034844c
Robin Beer <contact@robin-beer.com>
2022-12-18 23:22:15 +0100

    show current train progress on map and in progress bar

--- src/components/map-section/map-section.tsx
+++ src/components/map-section/map-section.tsx
@@ -13,6 +13,7 @@ import type { MapLocation } from "./map-location";
 
 import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from "../../../tailwind.config.cjs"
+import { findIndexOfClosestPoint } from "../../utils/coordinates";
 
 const tailwindConfigObj = resolveConfig(tailwindConfig)
 
@@ -32,20 +33,33 @@ export function MapSection(props: Props): React.ReactElement {
 
     const lineColorCode = tailwindConfigObj.theme?.colors[lineName.toLowerCase()]
 
-    const visibleRect = calculateVisibleRectFromPolyline(polylinePoints);
+    const region = calculateVisibleRegionFromPolyline(polylinePoints);
+
+    const currentLocationPolylinePoint = [currentLocation?.latitude || polylinePoints[0][0], currentLocation?.longitude || polylinePoints[0][1]];
+
+    const indexOfClosestPointOnLine = findIndexOfClosestPoint(currentLocationPolylinePoint, polylinePoints);
+
+    // also include current position as nearest point on line may be substantially behind train
+    // TODO: this does not account for a situation where the next point on the line is further than the train location
+    // this should be addressed in the future
+    const completedPolylinePoints = [...polylinePoints.slice(0, indexOfClosestPointOnLine), currentLocationPolylinePoint]
+
     return (
         <div className="h-96 w-11/12 lg:w-1/2 rounded-lg">
             <MapkitProvider tokenOrCallback={env.NEXT_PUBLIC_MAPKIT_TOKEN}>
-                <Map region={visibleRect} padding={12} showsUserLocation={true} isRotationEnabled={false} >
-                    <MapPolyline polylinePoints={polylinePoints} style={{ lineWidth: 5, strokeColor: lineColorCode }} />
-                    <Marker latitude={currentLocation?.latitude} longitude={currentLocation?.longitude} title="Train Location" />
+                <Map region={region} padding={12} showsUserLocation={true} isRotationEnabled={false} >
+                    {/* full line */}
+                    <MapPolyline polylinePoints={polylinePoints} style={{ lineWidth: 3, strokeColor: lineColorCode }} />
+                    {/* section of the line that has been completed */}
+                    <MapPolyline polylinePoints={completedPolylinePoints} style={{ lineWidth: 8, strokeColor: lineColorCode }} />
+                    {/* <Marker latitude={currentLocationPolylinePoint[0]} longitude={currentLocationPolylinePoint[1]} title="Train Location" /> */}
                 </Map>
             </MapkitProvider>
         </div >
     );
 }
 
-function calculateVisibleRectFromPolyline(polylinePoints: PolylinePoints) {
+function calculateVisibleRegionFromPolyline(polylinePoints: PolylinePoints) {
     const lats = polylinePoints.map(p => p[0]);
     const longs = polylinePoints.map(p => p[1]);
 
--- src/components/trip-sections/trip-section.tsx
+++ src/components/trip-sections/trip-section.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import type { Trip } from "../../server/trpc/models/trip";
+import { findIndexOfClosestPoint } from "../../utils/coordinates";
 import { formatTimeHHMM } from "../../utils/format-time";
 import { DelayText } from "../delay-text";
+import type { PolylinePoint, PolylinePoints } from "../map-section";
 
 interface Props {
-    trip: Trip
+    trip: Trip,
+    polylinePoints: PolylinePoints
 }
 
 export function TripSection(props: Props): React.ReactElement {
 
-    const { trip } = props;
+    const { trip, polylinePoints } = props;
+
+
+    const lineBgClass = `bg-${trip.lineName.toLowerCase()}`
+
+    const currentLocationPolylinePoint: PolylinePoint = [trip.currentLocation?.latitude || 0, trip.currentLocation?.longitude || 0];
+    const tripProgress = calculateTripProgress(polylinePoints, currentLocationPolylinePoint);
 
     return (
         <div className="flex flex-col items-start bg-white w-11/12 lg:w-1/2 min-h-min rounded-md my-4 p-4">
@@ -20,9 +29,9 @@ export function TripSection(props: Props): React.ReactElement {
                     <p>{trip.destination}</p>
                 </div>
                 <div className="w-full flex justify-center items-center">
-                    <div className="bg-red-700 h-2 w-2 rounded-full -mr-1 z-10"></div>
-                    <div className="bg-gray-900 h-1 w-4/5 rounded-full"></div>
-                    <div className="bg-red-700 h-2 w-2 rounded-full -ml-1 z-10"></div>
+                    <div className="bg-gray-400 w-4/5 h-3 rounded-full relative z-0">
+                        <div id="test" className={`${lineBgClass} h-3 rounded-full relative z-10`} style={{ width: `calc(${Math.floor(tripProgress * 100)}% - 4px)` }} />
+                    </div>
                 </div>
                 <div className="w-full flex justify-between items-center font-light text-sm px-5 py-2">
                     <p>{formatTimeHHMM(trip.departure)} {Boolean(trip.departureDelay) && <DelayText delay={trip.departureDelay} />}</p>
@@ -31,4 +40,9 @@ export function TripSection(props: Props): React.ReactElement {
             </div>
         </div>
     );
+}
+
+function calculateTripProgress(tripPolyline: PolylinePoints, currentLocation: PolylinePoint): number {
+    const closestPointIndex = findIndexOfClosestPoint(currentLocation, tripPolyline);
+    return closestPointIndex / tripPolyline.length;
 }
