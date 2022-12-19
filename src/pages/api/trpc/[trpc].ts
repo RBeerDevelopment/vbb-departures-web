4b221079b3aa0284d734a4aef4a27ab795f25532
Robin Beer <contact@robin-beer.com>
2022-12-20 00:23:58 +0100

    use edge functions for trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,10 @@ import { env } from "../../../env/server.mjs";
 import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
 
+export const config = {
+  runtime: 'edge'
+}
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
@@ -11,7 +15,7 @@ export default createNextApiHandler({
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
+        console.error(`❌ tRPC failed on ${path}: ${error}`);
+      }
       : undefined,
 });
