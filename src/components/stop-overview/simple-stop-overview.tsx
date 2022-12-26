89ba2b640f8d4dd1c31ec566257f035dd34b6c73
Robin Beer <contact@robin-beer.com>
2022-12-27 00:01:52 +0100

    add bg color ot station list

--- src/components/stop-overview/simple-stop-overview.tsx
+++ src/components/stop-overview/simple-stop-overview.tsx
@@ -6,23 +6,25 @@ import { StopState } from "./stop-state";
 interface Props {
     stop: Stopover,
     stopState: StopState
+    bgColor: string
 }
 
 export function SimpleStopOverview(props: Props): React.ReactElement {
 
-    const { stop, stopState } = props;
+    const { stop, stopState, bgColor } = props;
 
     console.log({ name: stop.stopName, stopState })
 
     return (
         <div className="flex flex-row gap-4 pl-4 h-16 items-center justify-between">
             <div className="w-3 h-full self-start items-center flex flex-col">
-                <div className="w-1 h-1/2 bg-red-700"/>
+                {(stopState === StopState.Past || stopState === StopState.Current) && 
+                    <div className={`w-1 h-1/2 ${bgColor}`}/>}
                 {stopState === StopState.Current ? 
-                    <div className="w-3 h-3 bg-red-700 rounded-full" />
+                    <div className={`w-3 h-3 ${bgColor} rounded-full`} />
                 : <></>}
                 {stopState === StopState.Past ? 
-                    <div className="w-1 h-1/2 bg-red-700" />
+                    <div className={`w-1 h-1/2 ${bgColor}`} />
                 : <></>}
             </div>
             <p>{stop.stopName}</p>
