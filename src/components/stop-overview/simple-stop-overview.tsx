42485e0be7e9c5b85d1c857fbbeca077a19d1f81
Robin Beer <contact@robin-beer.com>
2022-12-31 08:20:21 +0100

    change detail display

--- src/components/stop-overview/simple-stop-overview.tsx
+++ src/components/stop-overview/simple-stop-overview.tsx
@@ -19,21 +19,14 @@ export function SimpleStopOverview(props: Props): React.ReactElement {
             <div className="w-3 h-full self-start items-center flex flex-col">
                 {(stopState === StopState.Past || stopState === StopState.Current) && 
                     <div className={`w-1 h-1/2 ${bgColor}`}/>}
-                <div>
-                    {stopState === StopState.Current ? 
-                        <div className={`w-3 h-3 ${bgColor} rounded-full z-10`} />
-                    : <></>}
-                    {isSelectedStation ? 
-                        <div className="w-4 h-4 bg-blue-600 rounded-full flex justify-center items-center z-10" >
-                            <div className="w-2 h-2 bg-white rounded-full relative z-20" />
-                        </div>
-                    : <></>}
-                </div>
+                {stopState === StopState.Current ? 
+                    <div className={`w-3 h-3 ${bgColor} rounded-full z-10`} />
+                : <></>}
                 {stopState === StopState.Past ? 
                     <div className={`w-1 h-1/2 ${bgColor}`} />
                 : <></>}
             </div>
-            <p className="text-left w-1/2">{stop.stopName}</p>
+            <p className={`text-left w-1/2 ${isSelectedStation ? "text-red-600" : ""}`}>{stop.stopName}</p>
             <div className="flex flex-col w-1/3">
                 <TimeWithDelay time={stop.arrival} delay={stop.arrivalDelay} />
                 <TimeWithDelay time={stop.departure} delay={stop.departureDelay} />
