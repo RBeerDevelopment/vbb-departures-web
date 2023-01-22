b6e079b4ca2b8f6cd07d0b476fe6db036fc84bc5
Robin Beer <contact@robin-beer.com>
2023-01-22 15:37:31 +0100

    start using hafas client direcly for api

--- src/server/trpc/router/departures.ts
+++ src/server/trpc/router/departures.ts
@@ -1,32 +1,26 @@
-import axios from "axios";
 import { z } from "zod";
-import { env } from "@env/server.mjs";
-import type { DepatureResponse } from "../models/departure.js";
-import { mapDepartureResponseToDeparture } from "../models/departure";
 
 import { router, publicProcedure } from "../trpc";
+import { hafasClient } from "@utils/vbb-hafas/client";
+import { mapDepartureResponseToDeparture } from "../models/departure";
 
 export const departureRouter = router({
     byStationId: publicProcedure
         .input(z.object({ stationId: z.string(), resultCount: z.number().default(30), duration: z.number().default(30) }))
         .query(async ({ input }) => {
 
+
             const { stationId, resultCount, duration } = input;
 
-            const apiUrl = new URL(`${env.VBB_API_URL}stops/${stationId}/departures`);
+            const { departures } = await hafasClient.departures(stationId,
+                {
+                    remarks: false,
+                    results: resultCount,
+                    duration: duration,
+                }
+            );
 
-            apiUrl.searchParams.append("results", String(resultCount));
-            apiUrl.searchParams.append("duration", String(duration));
-            apiUrl.searchParams.append("ferry", "false");
-            apiUrl.searchParams.append("express", "false");
-            apiUrl.searchParams.append("regional", "false");
-            apiUrl.searchParams.append("pretty", "false");
-            apiUrl.searchParams.append("remarks", "false");
-
-            const resp = await axios.get<DepatureResponse[]>(apiUrl.toString());
-
-            const data = resp.data.map(mapDepartureResponseToDeparture);
-
+            const data = departures.map(mapDepartureResponseToDeparture);
             return data;
         }),
 });
