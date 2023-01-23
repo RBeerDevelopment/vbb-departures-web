74d9e36e07701c7ae5c15f085d93c7350cd1f261
Robin Beer <contact@robin-beer.com>
2023-01-23 21:31:41 +0100

    show time diff text instead of arrival time

--- src/components/departure-card.tsx
+++ src/components/departure-card.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import type { Departure } from "@server/trpc/models/departure";
 import { formatTimeHHMM } from "@utils/format-time";
 import { DelayText } from "./delay-text";
+import { formatMinDiffToNow } from "@utils/format-min-diff-to-now";
 
 interface Props {
     departure: Departure
@@ -21,12 +22,11 @@ export function DepartureCard(props: Props): React.ReactElement {
 
     return (
         <Link className="bg-white rounded-md w-11/12 md:w-1/2 py-3 px-2 my-2 flex flex-row gap-2" href={`/trip/${stationId}/${departure.id}/${departure.lineName}`}>
-
             <span className={`px-2 py-2 h-12 w-12 ${lineNameTextColor} text-center text-xl rounded-lg bg-${departure.lineName.toLowerCase()}`}>{departure.lineName}</span>
             <div className="flex flex-col overflow-hidden">
                 <p className="font-thin text-lg overflow-hidden whitespace-nowrap text-ellipsis">{departure.direction}</p>
                 <div className="flex flex-row gap-3">
-                    <p className="font-bold text-md">{formatTimeHHMM(departure.when)}</p>
+                    <p className="font-bold text-md">{formatMinDiffToNow(departure.when)}</p>
                     <DelayText delay={departure.delay} />
                 </div>
             </div>
---
+++ src/utils/format-min-diff.ts
@@ -0,0 +1,9 @@
+const formatter = new Intl.RelativeTimeFormat('en', { style: "narrow" });
+
+export function formatMinDiff(minDiff: number): string {
+    if (minDiff === 0) {
+        return "now";
+    }
+
+    return formatter.format(minDiff, "minutes");
+}
