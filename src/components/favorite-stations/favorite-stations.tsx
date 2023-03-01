1014036e968798d59b0e224705390b310963eea0
Robin Beer <contact@robin-beer.com>
2023-03-01 21:40:22 +0100

    prefetch first three favorite stations

--- src/components/favorite-stations/favorite-stations.tsx
+++ src/components/favorite-stations/favorite-stations.tsx
@@ -1,10 +1,13 @@
 import { favoriteStationAtom } from "@atoms/favorite-station-atom";
+import { usePrefetchFavoriteStations } from "@utils/prefetch-favorite-stations";
 import { useAtom } from "jotai";
 import React from "react";
 import { FavoriteStationCard } from "./favorite-station-card";
 
 export function FavoriteStations(): React.ReactElement {
   const [favoriteStations] = useAtom(favoriteStationAtom);
+
+  usePrefetchFavoriteStations();
 
   const stationCards = favoriteStations.map((fs) => (
     <FavoriteStationCard key={fs.id} station={fs} />
